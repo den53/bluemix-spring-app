2f7a57c1cca404942df671f204d59140fee269e0
Dennis Seidel <mail@dennisseidel.de>
2015-05-22 16:05:54 +0200

    Init Spring

---
+++ src/main/java/de/dennisseidel/bluemixspringapp/BluemixSpringAppApplication.java
@@ -0,0 +1,12 @@
+package de.dennisseidel.bluemixspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluemixSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluemixSpringAppApplication.class, args);
+    }
+}
---
+++ src/test/java/de/dennisseidel/bluemixspringapp/BluemixSpringAppApplicationTests.java
@@ -0,0 +1,18 @@
+package de.dennisseidel.bluemixspringapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BluemixSpringAppApplication.class)
+@WebAppConfiguration
+public class BluemixSpringAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
